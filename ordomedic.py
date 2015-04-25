4111cdfa2743116d668f1efc094e35eae2686500
whenrow <william.henrotin@gmail.com>
2015-04-25 14:41:04 +0200

    Sauvegarde ous dictionnaire

--- ordomedic.py
+++ ordomedic.py
@@ -4,9 +4,10 @@ from BeautifulSoup import BeautifulSoup
 # Url building
 site = "https://ordomedic.be/"
 town = "5000"
+numPage = "1"
 language = "fr"
 
-query = language+"/rechercher-un-medecin/?search_place="+town+"&page=1"
+query = language+"/rechercher-un-medecin/?search_place="+town+"&page="+numPage
 url = site+query
 
 # Url connecting
@@ -15,14 +16,19 @@ response = requests.get(url)
 # HTML parsing
 defaultSoup = BeautifulSoup(response.text)
 
+
 # Person list
-for med in defaultSoup.findAll(attrs = {"class": "result"}):
+people = []
+for info in defaultSoup.findAll(attrs = {"class": "result"}):
+  med = {}
   # Name's Extract
-  name = med.findAll(attrs = {"class": "name"})
+  name = info.findAll(attrs = {"class": "name"})
   # Address' extract
-  address = med.findAll(attrs = {"class": "address"})
+  address = info.findAll(attrs = {"class": "address"})
   # Text only of the info
-  print name[0].string
-  print address[0].findAll("dd",limit=2)[0].string
-  print address[0].findAll("dd",limit=2)[1].string
-  print ""
+  med["name"] = name[0].string
+  med["Address"] = address[0].findAll("dd",limit=2)[0].string + address[0].findAll("dd",limit=2)[1].string
+  med["Phone"] = address[0].findAll("dd",limit=2)[-1].string
+  people.append(med)
+
+print len(people)
