feb8835ffe0070e572201b88e5497b648e6f8833
whenrow <william.henrotin@gmail.com>
2015-04-26 20:48:53 +0200

    modif dans Telephone

--- ordomedic.py
+++ ordomedic.py
@@ -7,32 +7,46 @@ import json
 # Url building
 site = "https://ordomedic.be/"
 town = "5000"
-numPage = "1"
 language = "fr"
-
-query = language+"/rechercher-un-medecin/?search_place="+town+"&page="+numPage
-url = site+query
-
-# Url connecting
-response = requests.get(url)
-
-# HTML parsing
-defaultSoup = BeautifulSoup(response.text)
-
-
-# Person list
+numPage = 1
 people = []
-for info in defaultSoup.findAll(attrs = {"class": "result"}):
-  med = {}
-  # Name's Extract
-  name = info.findAll(attrs = {"class": "name"})
-  # Address' extract
-  address = info.findAll(attrs = {"class": "address"})
-  # Text only of the info
-  med["name"] = name[0].string
-  med["Address"] = address[0].findAll("dd",limit=2)[0].string + address[0].findAll("dd",limit=2)[1].string
-  med["Phone"] = address[0].findAll("dd")[-1].string.split(" ")[1]
-  people.append(med)
+isEnd = "N"
+
+while isEnd == "N":
+
+  query = language+"/rechercher-un-medecin/?search_place="+town+"&page="+str(numPage)
+  url = site+query
+
+  # Url connecting
+  response = requests.get(url)
+  print response.status_code
+
+  # HTML parsing
+  defaultSoup = BeautifulSoup(response.text)
+
+
+  # Person list
+  for info in defaultSoup.findAll(attrs = {"class": "result"}):
+    med = {}
+    # Name's Extract
+    name = info.findAll(attrs = {"class": "name"})
+    # Address' extract
+    address = info.findAll(attrs = {"class": "address"})
+    # Text only of the info
+    med["name"] = name[0].string
+    med["Address"] = address[0].findAll("dd",limit=2)[0].string + address[0].findAll("dd",limit=2)[1].string
+    # Extract of the phone number
+    phone = address[0].findAll("dd")[-1].string
+    if phone[1:3] == "Tel":
+      med["Phone"] = phone.split(" ")[1]
+    else
+      med["Phone"] = "None"
+    people.append(med)
+
+  # Add one to index
+  numPage = numPage+1
+  if numPage >= 20:
+    isEnd = "Y"
 
 # Save in a JSON file
 with open('medecins.json', 'w') as outfile:
