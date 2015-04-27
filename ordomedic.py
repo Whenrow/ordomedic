61722db9c4fba2b1f745884cfff87cb9c7fbe01a
whenrow <william.henrotin@gmail.com>
2015-04-27 21:00:33 +0200

    Ajout des specialisation

--- ordomedic.py
+++ ordomedic.py
@@ -9,57 +9,77 @@ def generateSpecialities():
   url = "https://ordomedic.be/fr/rechercher-un-medecin/"
   response = requests.get(url)
   soup = BeautifulSoup(response.text)
-  specList = soup.findAll("search_specialism")
-  sList = {}
-  for spec in specList:
-    sList.append(spec.string)
+  specList = soup.findAll(attrs={"name":"search_specialism"})
+  sList = []
+  for spec in specList[0].findAll("option")[1:]:
+    sList.append({"value":spec.get("value"),"name":spec.string})
 
   return sList
-
 
 # Url building
 site = "https://ordomedic.be/"
 town = "5000"
 language = "fr"
-numPage = 1
 people = []
+numberOfPeople = len(people)
 isEnd = "N"
 
-while isEnd == "N":
+for spec in generateSpecialities():
+  numPage = 1
+  while isEnd == "N":
 
-  query = language+"/rechercher-un-medecin/?search_place="+town+"&page="+str(numPage)
-  url = site+query
+    query = language+"/rechercher-un-medecin/?"
+    query = query + "search_specialism=" + spec["value"]
+    query = query + "search_place=" + town
+    query + query + "&page=" + str(numPage)
+    url = site+query
 
-  # Url connecting
-  response = requests.get(url)
-  print response.status_code
+    # Url connecting
+    response = requests.get(url)
 
-  # HTML parsing
-  defaultSoup = BeautifulSoup(response.text)
+    # Stop if bad connection
+    if response.status_code != 200:
+      print response.status_code
+      isEnd = "Y"
+      continue
+
+    # Or no more people to gather
+    if numberOfPeople == people:
+      isEnd = "Y"
+      print "too many people !"
+      break
+
+    # HTML parsing
+    defaultSoup = BeautifulSoup(response.text)
 
 
-  # Person list
-  for info in defaultSoup.findAll(attrs = {"class": "result"}):
-    med = {}
-    # Name's Extract
-    name = info.findAll(attrs = {"class": "name"})
-    # Address' extract
-    address = info.findAll(attrs = {"class": "address"})
-    # Text only of the info
-    med["name"] = name[0].string
-    med["Address"] = address[0].findAll("dd",limit=2)[0].string + address[0].findAll("dd",limit=2)[1].string
-    # Extract of the phone number
-    phone = address[0].findAll("dd")[-1].string
-    if phone[1:3] == "Tel":
-      med["Phone"] = phone.split(" ")[1]
-    else:
-      med["Phone"] = "None"
-    people.append(med)
+    # Person list
+    for info in defaultSoup.findAll(attrs = {"class": "result"}):
+      med = {}
+      # Name's Extract
+      name = info.findAll(attrs = {"class": "name"})
+      # Address' extract
+      address = info.findAll(attrs = {"class": "address"})
+      # Text only of the info
+      med["name"] = name[0].string
+      med["Address"] = str(address[0].findAll("dd",limit=2)[0].string) + str(address[0].findAll("dd",limit=2)[1].string)
+      # Extract of the phone number
+      phone = address[0].findAll("dd")[-1].string
+      if phone[1:3] == "Tel":
+        med["Phone"] = phone.split(" ")[1]
+      else:
+        med["Phone"] = "None"
+      # Add the specialisation
+      med["specialisation"] = spec["name"]
 
-  # Add one to index
-  numPage = numPage+1
-  if numPage >= 20:
-    isEnd = "Y"
+      # Not add duplicates
+      if med not in people:
+        people.append(med)
+
+    # Add one to index
+    numPage = numPage+1
+    if numPage >= 10:
+      isEnd = "Y"
 
 # Save in a JSON file
 with open('medecins.json', 'w') as outfile:
